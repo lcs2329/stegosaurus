b607194c4b2455ab61e4fb684e923d644ab9e027
nourishablegecko <lukesturgis227@gmail.com>
2019-11-04 15:47:15 -0500

    cleaned up imports

--- stegosaurus.py
+++ stegosaurus.py
@@ -1,82 +1,71 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
-
 import argparse
-import binascii
 import logging
 import os
-import struct
 
 import coloredlogs
 import numpy as np
-import scipy
-import scipy.cluster
-import scipy.misc
-from matplotlib.image import imread
 from PIL import Image
 
 log = logging.getLogger(__name__)
 coloredlogs.install(level="INFO", logger=log)
 
 
-def get_pixel_map(image:str, resize, resize_threshold:int=150):
-    """Get a pixel map from an image."""
+def get_image(image: str, resize:bool, resize_threshold: int = 150) -> Image:
+    """Convert an image into a pillow image object."""
     log.debug(f"Opening '{image}'...")
     try:
         img = Image.open(image, "r").convert("L")
     except:
         log.exception(f"Unable to open {image}. Exiting...")
         return
+
     if resize:
         img = img.resize((resize_threshold, resize_threshold))
-    
-    colors = Image.Image.getcolors(img)
-    topfive = sorted(colors, key=lambda t: t[0], reverse=True)[:5]
 
-    return np.asarray(img)
+    return img
 
 
-def rewrite_image(pixelmap):
+def get_pixel_map(image: Image) -> np.ndarray:
+    """Get a pixel map from an image."""
+    return np.asarray(image)
+
+
+def rewrite_image(pixelmap:np.ndarray):
     for pixel in pixelmap:
         log.debug(f"Old pixel: {pixel}")
-        for index,color in enumerate(pixel):
+        for index, color in enumerate(pixel):
             pixel[index] = color + 10
         log.debug(f"New pixel: {pixel}")
 
     # arr[arr > 255] = new_pixel
 
 
-def get_common_pixels(pixelmap, n=5):
+def get_common_pixels(image: Image, n:int=5) -> list:
     """Get the n most common pixel values."""
-    shape = pixelmap.shape
-    pixelmap = pixelmap.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
-
-    log.debug("Finding common clusters...")
-    codes, dist = scipy.cluster.vq.kmeans(pixelmap, n)
-    log.debug(f"Cluster centers: \n{codes}")
-
-    # assign codes
-    vecs, dist = scipy.cluster.vq.vq(pixelmap, codes)  
-
-    # count occurrences
-    counts, bins = scipy.histogram(vecs, len(codes))
-
-    # find most frequent    
-    index_max = scipy.argmax(counts)
-    peak = codes[index_max]
-
-    # get hex value
-    color = binascii.hexlify(bytearray(int(c) for c in peak)).decode("ascii")
-    log.debug(f"Most frequent is {peak} (#{color})")
-    return codes
+    colors = Image.Image.getcolors(image)
+    topfive = sorted(colors, key=lambda t: t[0], reverse=True)[:5]
+    log.debug(f"Top 5 most common pixel values: {topfive}")
+    return topfive
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Image Steganography tool")
     parser.add_argument("-i", "--image", type=str, required=True, help="Source image")
-    parser.add_argument("-v", "--verbose", action="store_true", default=False, help="Enable verbose logging.")
-    parser.add_argument("--resize", action="store_true", default=False, help="Resize image for faster processing.")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Enable verbose logging.",
+    )
+    parser.add_argument(
+        "--resize",
+        action="store_true",
+        default=False,
+        help="Resize image for faster processing.",
+    )
 
     args = parser.parse_args()
 
@@ -86,12 +75,14 @@ if __name__ == "__main__":
     if not os.path.exists(args.image):
         log.error(f"Image '{args.image}' not found.")
         exit(1)
-    
-    pixelmap = get_pixel_map(args.image, args.resize)
-    if pixelmap is None:
+
+    image = get_image(args.image, args.resize)
+    if image is None:
         exit()
 
-    common_pixels = get_common_pixels(pixelmap)
-    
+    pixelmap = get_pixel_map(image)
+   
+    common_pixels = get_common_pixels(image)
+
     for pixel in common_pixels:
         log.info(f"Most common pixels: {pixel}")
