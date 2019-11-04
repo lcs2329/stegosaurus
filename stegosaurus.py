d2409557955141b340d2821c4d2097bf51f5ed18
nourishablegecko <lukesturgis227@gmail.com>
2019-11-04 15:55:51 -0500

    some more docs 'n stuff

--- stegosaurus.py
+++ stegosaurus.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 coloredlogs.install(level="INFO", logger=log)
 
 
-def get_image(image: str, resize:bool, resize_threshold: int = 150) -> Image:
+def get_image(image: str, resize: bool, resize_threshold: int = 150) -> Image:
     """Convert an image into a pillow image object."""
     log.debug(f"Opening '{image}'...")
     try:
@@ -21,6 +21,8 @@ def get_image(image: str, resize:bool, resize_threshold: int = 150) -> Image:
         log.exception(f"Unable to open {image}. Exiting...")
         return
 
+    # resizing makes it faster to work with an image, but may decrease
+    # image quality. we'll have to play around with it
     if resize:
         img = img.resize((resize_threshold, resize_threshold))
 
@@ -32,26 +34,31 @@ def get_pixel_map(image: Image) -> np.ndarray:
     return np.asarray(image)
 
 
-def rewrite_image(pixelmap:np.ndarray):
+def rewrite_image(pixelmap: np.ndarray):
+    """Rewrite individual pixels within an image."""
     for pixel in pixelmap:
         log.debug(f"Old pixel: {pixel}")
         for index, color in enumerate(pixel):
             pixel[index] = color + 10
         log.debug(f"New pixel: {pixel}")
 
+    # apparently this is a much faster way to do direct edits to
+    # a numpy ndarray, rather than pixel by pixel
     # arr[arr > 255] = new_pixel
 
 
-def get_common_pixels(image: Image, n:int=5) -> list:
+def get_common_pixels(image: Image, n: int = 5) -> list:
     """Get the n most common pixel values."""
     colors = Image.Image.getcolors(image)
-    topfive = sorted(colors, key=lambda t: t[0], reverse=True)[:5]
+
+    # frequency: pixel value (0-255)
+    topfive = sorted(colors, key=lambda t: t[0], reverse=True)[:n]
     log.debug(f"Top 5 most common pixel values: {topfive}")
     return topfive
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Image Steganography tool")
+    parser = argparse.ArgumentParser(description="Hide messages within image files.")
     parser.add_argument("-i", "--image", type=str, required=True, help="Source image")
     parser.add_argument(
         "-v",
@@ -80,9 +87,6 @@ if __name__ == "__main__":
     if image is None:
         exit()
 
-    pixelmap = get_pixel_map(image)
-   
     common_pixels = get_common_pixels(image)
 
-    for pixel in common_pixels:
-        log.info(f"Most common pixels: {pixel}")
+    pixelmap = get_pixel_map(image)
