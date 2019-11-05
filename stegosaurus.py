f9610aa9a3193052f6eb1af60deec93fd6efb3df
nourishablegecko <lukesturgis227@gmail.com>
2019-11-04 20:30:20 -0500

    correct modification

--- stegosaurus.py
+++ stegosaurus.py
@@ -6,27 +6,32 @@ import os
 
 import coloredlogs
 import numpy as np
-import png
 from PIL import Image
 
 log = logging.getLogger(__name__)
-coloredlogs.install(level="INFO", logger=log)
+coloredlogs.install(
+    level="INFO", fmt="[%(asctime)s] [%(levelname)-8s] %(message)s'", logger=log
+)
 
 
-def open_image(path):
-    """Open an Image."""
-    log.debug(f"Opening image at path '{path}'...")
-    try:
-        newImage = Image.open(path, "r").convert("L")
-    except:
-        log.exception(f"Unable to open image at path '{path}'.")
+def open_image(image_path: str) -> Image:
+    """Open an image."""
+    if not os.path.exists(image_path):
+        log.error(f"Image '{args.source}' not found.")
         return
 
-    return newImage
+    log.debug(f"Opening image at path '{image_path}'...")
+    try:
+        image = Image.open(image_path, "r")
+    except:
+        log.exception(f"Unable to open image at path '{image_path}'.")
+        return
+
+    return image
 
 
-def save_image(image: Image, path: str):
-    """Save an Image."""
+def save_image(image: Image, path: str) -> None:
+    """Save an image."""
     log.debug(f"Saving image to '{path}'...")
     try:
         image.save(path, "png")
@@ -38,7 +43,7 @@ def create_image(x: int, y: int) -> Image:
     """Create a new image with the given size."""
     log.debug(f"Creating new image of size {x}, {y}...")
     try:
-        image = Image.new("RGB", (x, y), "white")
+        image = Image.new("RGB", (x, y))
     except:
         log.exception("Unable to create new image.")
         return
@@ -61,29 +66,48 @@ def get_pixel(image: Image, x: int, y: int) -> tuple:
 
 def get_common_pixels(image: Image, n: int = 5) -> list:
     """Get the n most common pixel values."""
-    colors = Image.Image.getcolors(image)
+    colors = Image.Image.getcolors(image, maxcolors=image.size[0] * image.size[1])
 
     # frequency: pixel value (0-255)
-    topfive = sorted(colors, key=lambda t: t[0], reverse=True)[:n]
-    log.debug(f"Top {n} most common pixel values: {topfive}")
-    return topfive
+    top_five = sorted(colors, key=lambda t: t[0], reverse=True)[:n]
+    log.debug(f"Top {n} most common pixel values: {top_five}")
+    return top_five
+
+
+def transform_pixels(image: Image, pixel_value: tuple, shift: int) -> Image:
+    """Modify a given image."""
+    width, height = image.size
+
+    # create new image & pixel map
+    new_image = create_image(width, height)
+    pixels = new_image.load()
+
+    for x in range(width):
+        for y in range(height):
+            pixel = get_pixel(image, x, y)
+
+            red = pixel[0]
+            green = pixel[1]
+            blue = pixel[2]
+
+            if pixel == pixel_value:
+                # if we have a match for our desired modified pixel, then
+                # set the pixel in the new image accordingly
+                red = red * shift // 255
+                green = red * shift // 255
+                blue = red * shift // 255
+            
+            # set pixel in new image
+            pixels[x, y] = (int(red), int(green), int(blue))
+
+    return new_image
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Hide messages within image files.")
+    parser.add_argument("-s", "--source", type=str, required=True, help="Source image")
     parser.add_argument(
-        "-s", 
-        "--source", 
-        type=str, 
-        required=True, 
-        help="Source image"
-    )
-    parser.add_argument(
-        "-d",
-        "--destination",
-        type=str,
-        default=None,
-        help="Destination image"
+        "-d", "--destination", type=str, default=None, help="Destination image"
     )
     parser.add_argument(
         "-v",
@@ -96,14 +120,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.verbose:
-        coloredlogs.install(level="DEBUG", logger=log)
-
-    if not os.path.exists(args.source):
-        log.error(f"Image '{args.source}' not found.")
-        exit(1)
-
+        coloredlogs.install(
+            level="DEBUG",
+            fmt="[%(asctime)s] [%(levelname)-8s] %(message)s'",
+            logger=log,
+        )
+    
     image = open_image(args.source)
+    if not image:
+        exit(1)
 
     common_pixels = get_common_pixels(image)
 
-    save_image(image, args.destination or f"new.{args.source}")
+    new_image = transform_pixels(image, common_pixels[0][1], 2)
+
+    save_image(new_image, args.destination or f"new.{args.source}")
+
+    log.info("All steps completed.")
